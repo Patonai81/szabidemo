cfe77729e38cf74a85f41da903979523662758f5
Patonai Szabolcs <patonai.szabolcs@gmail.com>
2020-03-22 11:07:23 +0100

    Első springes alkalmazásom

---
+++ src/main/java/hu/cib/szabi/szabidemo/SzabidemoApplication.java
@@ -0,0 +1,13 @@
+package hu.cib.szabi.szabidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzabidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzabidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/cib/szabi/szabidemo/SzabidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cib.szabi.szabidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzabidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
