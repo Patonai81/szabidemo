86e6864e5921a32170c0e4c99907a42a773630c8
Patonai Szabolcs <patonai.szabolcs@gmail.com>
2020-03-22 15:25:39 +0100

    Itt már működik a Flapdoodle alkalmazás is

--- src/main/java/hu/cib/szabi/szabidemo/SzabidemoApplication.java
+++ src/main/java/hu/cib/szabi/szabidemo/SzabidemoApplication.java
@@ -1,10 +1,22 @@
 package hu.cib.szabi.szabidemo;
 
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SzabidemoApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SzabidemoApplication.class, args);
