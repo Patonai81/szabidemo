0c6f0811b13d1b084ecba1aadef28e01c20a852f
Patonai Szabolcs <patonai.szabolcs@gmail.com>
2020-03-24 18:08:36 +0100

    Auto konfiguráció? Ha nem kapcsolom be nem találja a reactive web containert...

--- src/main/java/hu/cib/szabi/szabidemo/repository/ChapterRepository.java
+++ src/main/java/hu/cib/szabi/szabidemo/repository/ChapterRepository.java
@@ -2,7 +2,9 @@ package hu.cib.szabi.szabidemo.repository;
 
 import hu.cib.szabi.szabidemo.dto.Chapter;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface ChapterRepository extends ReactiveCrudRepository<Chapter,String> {
 }
